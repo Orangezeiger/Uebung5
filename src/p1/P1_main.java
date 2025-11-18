fc75b4625b2e58ba707fa74bd62828879d0bbdf6
Max <maxsiepsiak238@gmail.com>
2025-11-18 10:36:33 +0100

    Init p1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class P1_main {
+    public static void main(String[] args) {
+        for(int i=1;i<=11;i++){
+            System.out.println(i);
+        }
+        int x = 0;
+        do{
+            System.out.println(x);
+            x++;
+        }while (x <= 11);
+    }
+}
