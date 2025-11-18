9809dc45a1def496b67f0bcbae5e4289a6c1aabc
Max <maxsiepsiak238@gmail.com>
2025-11-18 10:44:30 +0100

    Init p2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,24 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int[] numbers = {23, 88,-10,55,39, -200,-1};
+        int max = 12;
+        int minIndex = 3;
+
+        max = numbers[0];
+        minIndex = 0;
+        for (int i = 0; i < numbers.length; i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+
+            if(numbers[i] < numbers[minIndex]){
+                minIndex = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(minIndex);
+    }
+}
